b189b1de712b2047c8cdca71fa3a177b44dfe1df
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-02-21 16:35:19 -0300

    fix: correções na formatação do código

--- fundamentos-react/src/components/basicos/Fragmento.jsx
+++ fundamentos-react/src/components/basicos/Fragmento.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function Fragmento(props) {
     return (
         <>
--- fundamentos-react/src/components/basicos/Primeiro.jsx
+++ fundamentos-react/src/components/basicos/Primeiro.jsx
@@ -1,6 +1,6 @@
 export default function Primeiro() {
-    const msg = 'Seja bem-vindo(a)!'
-    
+    const msg = 'Seja bem-vindo(a)!';
+
     return (
         <div>
             <h2>Primeiro Componente</h2>
