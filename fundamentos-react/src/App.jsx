6c8c5909b2497d132d0ee2ee49258b0202e9b05c
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-02-21 16:46:52 -0300

    feat: desafio do Componente gerador de números aleatórios

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,10 +1,12 @@
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
 import Fragmento from './components/basicos/Fragmento';
+import Aleatorio from './components/basicos/Aleatorio';
 
 const App = () => (
     <div id="app">
         <h1>Fundamentos React (Arrow _)</h1>
+        <Aleatorio min={0} max={10} />
         <Fragmento />
         <ComParametro aluno="Pedro Silva" nota={8.5} />
         <Primeiro />
---
+++ fundamentos-react/src/components/basicos/Aleatorio.jsx
@@ -0,0 +1,30 @@
+// Função que retorna um número aleatório inteiro localizado entre os valores dos parâmetros "min" e "max".
+// É um gerador inclusivo, então os valores de "min" e "max" também são possíveis valores a serem sorteados.
+const getNumAleatorio = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.round(Math.random() * (max - min) + min);
+};
+
+export default function Aleatorio(props) {
+    const { min, max } = props;
+    const numAleatorio = getNumAleatorio(min, max);
+
+    return (
+        <>
+            <h2>Sorteador de números aleatórios</h2>
+            <p>
+                <strong>Valor Mínimo: </strong>
+                {min}
+            </p>
+            <p>
+                <strong>Valor Máximo: </strong>
+                {max}
+            </p>
+            <p>
+                <strong>Valor Sorteado: </strong>
+                {numAleatorio}
+            </p>
+        </>
+    );
+}
