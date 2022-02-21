44e0fbb81422dd4b013c8adde7062ffd74cb19e8
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-02-21 16:15:23 -0300

    feat: envolver grupos de elementos JSX com Fragments
    Para saber mais: https://reactjs.org/docs/fragments.html

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,13 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
+import Fragmento from './components/basicos/Fragmento';
 
 ReactDOM.render(
-    <div>
+    <div id="app">
         <Primeiro />
         <ComParametro 
             aluno="Pedro Silva" 
             nota={8.5} />
+        <Fragmento />
     </div>,
     document.getElementById('root')
 );
