1900bbc8790b9c6e4b6f9ac8f67ed8eff3db5355
Marcel Barbosa <marcel.sby.br@gmail.com>
2022-02-22 16:56:44 -0300

    feat: repetições em React (listas)

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -7,12 +7,16 @@ import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
 import Familia from './components/basicos/Familia';
 import FamiliaMembro from './components/basicos/FamiliaMembro';
+import ListaAlunos from './components/repeticao/ListaAlunos';
 
 const App = () => (
     <div className="App">
         <h1>Fundamentos React</h1>
 
         <div className="Cards">
+            <Card titulo="#06 - Repetição" color="#32533D">
+                <ListaAlunos />
+            </Card>
             <Card titulo="#05 - Componente com Filhos" color="#904E55">
                 <Familia sobrenome="Silva">
                     <FamiliaMembro nome="Pedro" />
